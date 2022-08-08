08bf9573f2dd723c4efa45718a65e4d16bd1feb4
Crowdbotics <team@crowdbotics.com>
2022-08-08 03:57:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "niles_smoke_shop_ll_36656",
+  url: "https://niles_smoke_shop_ll_36656.botics.co",
+  api: "https://niles_smoke_shop_ll_36656.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
